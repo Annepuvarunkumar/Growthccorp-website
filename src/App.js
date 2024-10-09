7ba75bbf57d0f814a9b741b1d4fc38ef2ea3d179
Anshul Jayaswal <anshuljayaswal23@gmail.com>
2024-10-09 11:24:54 +0530

    Changes updated part 4

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, World!</h1>
+      <h1>This is Anshul</h1>
+      <h1>This project is for buisness optima for launching a website named medhealthplatform</h1>
+    </div>
+  );
+}
+
+export default App;
