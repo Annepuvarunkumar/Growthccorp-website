cd8829892cb79d1635d64b6ad375f6c7349d5469
varun <varunannepu123@gmail.com>
2024-10-10 14:18:28 +0530

    growthccorp-changes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Hello, World!</h1>
-      <h1>This is Anshul</h1>
-      <h1>This project is for buisness optima for launching a website named medhealthplatform</h1>
+      <h1>This is Varun</h1>
+      <h1>This project is for buisness optima for launching a website named growthccorp</h1>
     </div>
   );
 }
